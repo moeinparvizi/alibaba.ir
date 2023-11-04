48b2db0b670ab44586b732a8a53a35564dde2e3f
moeinparvizi <giventulip@gmail.com>
2023-11-04 17:01:14 +0330

    start alibaba project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { ArrowDown } from 'react-bootstrap-icons'
+
+
+function App() {
+
+  return (
+    <>
+    <p className='text-[40px]'><ArrowDown /></p>
+    </>
+  )
+}
+
+export default App
