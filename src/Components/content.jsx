d37f3c5aa1a1059adabcb82c217eb08c44f56e5b
moeinparvizi <giventulip@gmail.com>
2023-11-07 21:54:38 +0330

    complate banner changer

--- src/Components/content.jsx
+++ src/Components/content.jsx
@@ -1,6 +1,12 @@
 // import React from 'react'
 // banners
-import bannerAirplanin from "../assets/images/hero-e1fa22fb.webp";
+import bannerAirplanin from "../assets/images/banner/hero-6061caed.jpg";
+import bannerAirplanout from "../assets/images/banner/hero-9cf704bc.jpg";
+import bannerTrain from "../assets/images/banner/hero-f5969150.webp";
+import bannerBus from "../assets/images/banner/hero-824e4df4.webp";
+import bannerToor from "../assets/images/banner/hero-f039b6c5.webp";
+import bannerHotel from "../assets/images/banner/hero-ea8b776d.jpg";
+import bannerVilla from "../assets/images/banner/hero-2aa84383.jpg";
 
 // svgs
 import ParvazeDakheliIcon from "./icons/parvazedakheli";
@@ -11,10 +17,9 @@ import ToorIcon from "./icons/toor";
 import HotelIcon from "./icons/hotel";
 import VillaIcon from "./icons/villa";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Tabs() {
-  // choices a tab and change color and more
   function resetTab(tab) {
     const tabs = document.querySelectorAll(".tab");
     tabs.forEach((item) => {
@@ -45,6 +50,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[0];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerAirplanin)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -66,6 +73,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[1];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerAirplanout)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -87,6 +96,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[2];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerTrain)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -108,6 +119,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[3];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerBus)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -129,6 +142,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[4];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerToor)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -150,6 +165,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[5];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerHotel)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -171,6 +188,8 @@ function Tabs() {
         onClick={() => {
           const tab = document.querySelectorAll(".tab")[6];
           resetTab(tab);
+          document.querySelector('.banner').removeAttribute('src')
+          document.querySelector('.banner').setAttribute('src',bannerVilla)
         }}
       >
         <figure className="text-blue my-2 font-bold">
@@ -193,11 +212,12 @@ function Tabs() {
 }
 
 function Content() {
+
   return (
     <section className="flex justify-center px-20">
       <div className="container">
         <figure className="w-full flex justify-center flex-col">
-          <img src={bannerAirplanin} alt="banner" />
+          <img className="banner" src={bannerAirplanin} alt="banner" />
           <figcaption className="border-2 w-10/12 rounded-md border-line translate-x-[-10%] translate-y-[-62%] bg-white">
             <Tabs />
             <hr className="border border-line" />
